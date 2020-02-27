9fa7d8ae307ef5c205f0c5fc0dccd2ed285b1d9b
sheldontheking <wei980622@gmail.com>
2020-02-26 18:11:09 -0600

    starting the pages state change

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,19 +1,26 @@
-let button_1, img_1;
+let button_1, img_home;
+let btn_home, btn_add, btn_act, btn_prof, btn_search;
 
 function preload() {
-  img_1 = loadImage('assets/IMG_4061.jpg');
+  img_home = loadImage('assets/IMG_2021.PNG');
 }
 
 function setup() {
   createCanvas(900, 1600);
   // img_1 = loadImage('IMG_4061.jpg');
 
-  button_1 = createButton('load gallery');
-  button_1.position(10,10);
-  button_1.mousePressed(loadPic);
+  // button_1 = createButton('load gallery');
+  // button_1.position(10,10);
+  // button_1.mousePressed(loadPic);
+
+  btn_home = createImg('assets/home.png');
+  btn_home.position(10,1500);
+  btn_home.mousePressed(load_home);
+  // image(img_home, 0, 0, 900, 1600);
 }
 
-function loadPic() {
-  image(img_1, 0, 0);
+function load_home() {
   // put drawing code here
+  image(img_home, 0, 0, 900, 1600);
+
 }
