06c9aaaae5bb197a6d8c93e876da60812ddb3a1e
ColbyMainard <55711813+ColbyMainard@users.noreply.github.com>
2020-03-03 23:04:26 -0600

    Update to sketch
    More buttons with actions defined.  Will need to define images for these actions later.

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,4 +1,4 @@
-let button_1, img_home;
+let img_home, img_home, img_add, img_act, img_prof, img_search;
 let btn_home, btn_add, btn_act, btn_prof, btn_search;
 
 function preload() {
@@ -7,20 +7,45 @@ function preload() {
 
 function setup() {
   createCanvas(900, 1600);
-  // img_1 = loadImage('IMG_4061.jpg');
-
-  // button_1 = createButton('load gallery');
-  // button_1.position(10,10);
-  // button_1.mousePressed(loadPic);
+  load_home();
 
   btn_home = createImg('assets/home.png');
-  btn_home.position(10,1500);
+  btn_home.position(0,1500);
   btn_home.mousePressed(load_home);
-  // image(img_home, 0, 0, 900, 1600);
+
+  btn_search = createImg('assets/search.png');
+  btn_search.position(180,1500);
+  btn_search.mousePressed(load_search);
+
+  btn_add = createImg('assets/add.png');
+  btn_add.position(360,1500);
+  btn_add.mousePressed(load_add);
+
+  btn_act = createImg('assets/activity.png');
+  btn_act.position(540,1500);
+  btn_act.mousePressed(load_act);
+
+  btn_prof = createImg('assets/profile.png');
+  btn_prof.position(720,1500);
+  btn_prof.mousePressed(load_prof);
 }
 
 function load_home() {
-  // put drawing code here
   image(img_home, 0, 0, 900, 1600);
+}
 
+function load_search(){
+  image(img_search, 0, 0, 900, 1600);
+}
+
+function load_add(){
+  image(image_add, 0, 0, 900, 1600);
+}
+
+function load_prof(){
+  image(image_prof, 0, 0, 900, 1600);
+}
+
+function load_act(){
+  image(image_act, 0, 0, 900, 1600);
 }
